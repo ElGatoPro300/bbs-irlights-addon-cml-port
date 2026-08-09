df190a9b6f5066b24d59e002ca1a2122b8f29b4e
ElGatoPro300 <catgamer207@gmail.com>
2026-08-09 06:50:22 -0600

    Revert "para despues"
    This reverts commit b5db58bff64b59e8dc0b238539f970ab622d310f.

--- src/client/java/qualet/irlite/mixin/client/bbs/IFormRenderingContext.java
+++
@@ -1,9 +0,0 @@
-package qualet.irlite.mixin.client.bbs;
-
-import net.minecraft.client.util.math.MatrixStack;
-
-public interface IFormRenderingContext
-{
-    MatrixStack irlite$getWorld();
-    void irlite$setWorld(MatrixStack world);
-}
